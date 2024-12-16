a3244aba06673aaaa9094be3b413ca40d2772084
Yuiheron <153649080+Yuiheron@users.noreply.github.com>
2024-12-16 12:15:54 +0800

    Add files via upload

---
+++ game.js
@@ -0,0 +1,95 @@
+const buttonColours = ["red","blue","green","yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let gameStart = false;
+let level = 0;
+
+function playSound(name){
+  $("#" + name).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  const audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor){
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function(){
+  $("#" + currentColor).removeClass("pressed");
+  },100);
+}
+
+function nextSequence(){
+  //Clear User answers
+  userClickedPattern = [];
+
+  const randomNumber = Math.floor(Math.random() * 4);
+  const randomChosenColor = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  //this one plays the end only
+  // playSound(gamePattern[gamePattern.length - 1]);
+
+  //plays the entire sequence again
+  let delay = 0;
+  gamePattern.forEach(function(color, index) {
+    setTimeout(function() {
+      playSound(color);
+    }, delay);
+    delay += 500;  // Increase delay by 500ms (0.5 second) for each sound
+  });
+
+  level++; 
+  $("h1").text("Level " + level);
+};
+
+function startOver(){
+  level = 0
+  gamePattern = [];
+  userClickedPattern = [];
+  gameStart = false;
+}
+
+function checkAnswer(currentlevel){
+  if(userClickedPattern[currentlevel] === gamePattern[currentlevel]){
+    console.log("Success");
+
+    if(userClickedPattern.length === gamePattern.length){
+      setTimeout(function(){
+        nextSequence();
+      },1000);
+    }
+  }else{
+    //if user got the answer wrong
+    console.log("wrong");
+
+    playSound("wrong");
+
+    //change background when answer is wrong!
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    },200);
+
+    //Change the Title
+    $("h1").text("Game Over, Press A to restart!");
+
+    //Call the function if the player wants to reset
+    startOver();
+  }
+}
+
+$(".btn").on("click",function(){
+  const userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).on("keyup", function(event){
+  if (event.key === "a"){
+    nextSequence();
+    gameStart = true;
+  }
+})
